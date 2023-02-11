23a91dcd452aeb41a6b52ed7efef62b1a971f6ed
Zebreus <lennarteichhorn@googlemail.com>
2023-02-11 16:00:54 +0100

    Add tang 9k example

---
+++ hardware-tang/counter.v
@@ -0,0 +1,20 @@
+module top
+(
+    input clk,
+    output [5:0] led
+);
+
+reg [23:0] clockCounter = 0;
+localparam WAIT_TIME = 13500000;
+reg [5:0] ledCounter = 0;
+
+always @(posedge clk) begin
+    clockCounter <= clockCounter + 1;
+    if (clockCounter == WAIT_TIME) begin
+        clockCounter <= 0;
+        ledCounter <= ledCounter + 1;
+    end
+end
+
+assign led = ~ledCounter;
+endmodule
