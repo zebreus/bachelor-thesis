943fea808a5f204c7e894442538c00d982132330
Zebreus <lennarteichhorn@googlemail.com>
2023-03-02 11:27:45 +0100

    Add check for correct result to md5 driver

--- hardware-tang/driver_md5.v
+++ hardware-tang/driver_md5.v
@@ -7,6 +7,8 @@ module top
         output reg [7:0] logic_analyzer
     );
 
+    initial led = 0;
+    initial logic_analyzer = 0;
 
     reg [7:0] slowCounter = 0;
     always @(posedge clk) begin
@@ -21,9 +23,9 @@ module top
     wire [63:0] current_write_data;
     wire [11:0] current_data_size;
 
-    reg [63:0] current_read_data;
+    reg [63:0] current_read_data = 0;
 
-    reg start = 0;
+    wire start;
     wire [1:0] data_ready;
 
     md5 state_test_inst (
@@ -32,6 +34,7 @@ module top
         .start_port(start),
         .Pd458(32'h40000000),
         .Pd459(32'h40000100),
+        .Pd460(32'h40000200),
         .M_Rdata_ram(current_read_data),
         .M_DataRdy(data_ready),
 
@@ -47,18 +50,20 @@ module top
 
     reg [10:0] done_countdown;
 
+    reg [1:0] received_correct_hash = 0;
+
+    assign start = done_countdown == 0 && done != 1 ? 1 : 0;
+
     always @(posedge slowClock) begin
         if(done == 1) begin
-            done_countdown <= 100;
-            start <= 0;
+            done_countdown <= 200;
         end
         else begin
             if (done_countdown != 0) begin
                 done_countdown <= done_countdown - 1;
-                start <= 0;
             end
             else begin
-                start <= 1;
+                done_countdown <= 0;
             end
         end
 
@@ -76,14 +81,32 @@ module top
             current_read_data[63:32] <= 32'h00000000;
         end
 
-        if (write_enable != 0) begin
-            led <= 6'b010101;
-            logic_analyzer <= current_write_data[39:32];
-            // logic_analyzer <= current_write_data[7:0];
+        if (write_enable == 3) begin
+            if(current_write_data == 64'h04b2008fd98c1dd4)
+                received_correct_hash[0] <= 1;
+            else
+                received_correct_hash[0] <= received_correct_hash[0];
 
+            if(current_write_data == 64'h7e42f8ec980980e9)
+                received_correct_hash[1] <= 1;
+            else
+                received_correct_hash[1] <= received_correct_hash[1];
+        end
+
+        if (received_correct_hash = 2'b11) begin
+            led <= 6'b100111;
+            done_forever <= 1;
         end
         else begin
-            led <= 6'b000111;
+            led <= 6'b010000;
+            done_forever <= 0;
+        end
+
+        if (write_enable != 0) begin
+            logic_analyzer <= current_write_data[39:32];
+            // logic_analyzer <= current_write_data[7:0];
+        end
+        else begin
             logic_analyzer <= 8'h00000000;
         end
     end
