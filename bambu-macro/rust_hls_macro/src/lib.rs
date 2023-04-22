571ab6b378d4089429691932dca6c5d022a622a8
Zebreus <lennarteichhorn@googlemail.com>
2023-04-22 12:49:12 +0200

    Document limitations of the rust hls macro

--- bambu-macro/rust_hls_macro/src/lib.rs
+++ bambu-macro/rust_hls_macro/src/lib.rs
@@ -1,4 +1,16 @@
 //! Provides a [`hls!`] macro that can be used to synthesize a function into a rust-hdl module
+//!
+//! This is crate is highly experimental.
+//!
+//! It requires some things to work properly:
+//! * The function must be public
+//! * The function must be #[no_mangle]
+//! * All imported things of the function should be fully qualified
+//! * The function should not import things from its own crate. Other crates are fine
+//! * Your crate needs to have `rust-hdl` and `wrap-verilog-in-rust-hdl-macro` as dependencies
+//! * You need to have panda bambu installed and the binary as `bambu` in your path
+//! * You should have bash installed
+//! * Probably more things I did not think about
 
 #![feature(proc_macro_span)]
 #[cfg(feature = "perform-hls")]
