eba60f6fb8a9277a2626cee81eb67c6db5a2136b
Zebreus <lennarteichhorn@googlemail.com>
2023-06-05 11:04:22 +0200

    Fix nix build

--- flake.nix
+++ flake.nix
@@ -11,10 +11,9 @@
     flake-utils.url = "github:numtide/flake-utils";
     symbolator.url = "github:zebreus/symbolator";
     vcd2wavedrom.url = "github:zebreus/vcd2wavedrom";
-    llvm16-nixpkgs.url = "github:RaitoBezarius/nixpkgs?ref=2c627d9c702202d75746fd45045d20008bf7ed86";
   };
 
-  outputs = { self, nixpkgs, fenix, flake-utils, symbolator, vcd2wavedrom, unstable-nixpkgs, llvm16-nixpkgs }:
+  outputs = { self, nixpkgs, fenix, flake-utils, symbolator, vcd2wavedrom, unstable-nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
@@ -33,8 +32,8 @@
         packages.expose = import ./expose/default.nix { pkgs = pkgs; symbolator = symbolator.packages.${system}.symbolator; };
         packages.writing = import ./writing/default.nix { pkgs = pkgs; symbolator = symbolator.packages.${system}.symbolator; };
         packages.toolchain = import ./toolchain-test/default.nix { pkgs = pkgs; };
-        packages.bambu-unwrapped = import ./bambu/default.nix { pkgs = llvm16-nixpkgs.legacyPackages.${system}; };
-        packages.bambu-wrapped = import ./bambu/wrapped.nix { pkgs = llvm16-nixpkgs.legacyPackages.${system}; bambu = packages.bambu-unwrapped; };
+        packages.bambu-unwrapped = import ./bambu/default.nix { pkgs = unstable-nixpkgs.legacyPackages.${system}; };
+        packages.bambu-wrapped = import ./bambu/wrapped.nix { pkgs = unstable-nixpkgs.legacyPackages.${system}; bambu = packages.bambu-unwrapped; };
         packages.bambu = packages.bambu-wrapped;
         packages.bambu-appimage = import ./bambu/appimage.nix { pkgs = nixpkgs.legacyPackages.${system}; };
         packages.hardware-example = import ./hardware-example/default.nix { pkgs = pkgs; };
